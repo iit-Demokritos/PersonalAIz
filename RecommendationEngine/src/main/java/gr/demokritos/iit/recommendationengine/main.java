1f321ab6213ed9fd48145db404becde000ebd1e2
panagiotis <panagiotis@panagiotis-iit.demokritos.gr>
2015-02-13 11:59:06 +0200

    Init project

---
+++ RecommendationEngine/src/main/java/gr/demokritos/iit/recommendationengine/main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.demokritos.iit.recommendationengine;
+
+/**
+ *
+ * @author Giotis Panagiotis <giotis.p@gmail.com>
+ */
+public class main {
+    
+}
