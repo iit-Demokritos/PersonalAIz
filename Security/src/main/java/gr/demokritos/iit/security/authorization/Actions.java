1fbbbc59fb4975947ca2d3ebdcea1ca42f719fbe
panagiotis <panagiotis@panagiotis-iit.demokritos.gr>
2015-04-01 17:31:40 +0300

    add authorization class

---
+++ Security/src/main/java/gr/demokritos/iit/security/authorization/Actions.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.demokritos.iit.security.authorization;
+
+/**
+ *
+ * @author Giotis Panagiotis <giotis.p@gmail.com>
+ */
+public class Actions {
+
+    public static final String adminPanel = "";
+    
+    
+    public static final String createClients = "CreateClients";
+    public static final String readClients = "ReadClients";
+    public static final String updateClients = "UpdateClients";
+    public static final String deleteClients = "DeleteClients";
+    
+    public static final String createStereotypes = "CreateStereotypes";
+    public static final String readStereotypes = "ReadStereotypes";
+    public static final String updateStereotypes = "UpdateStereotypes";
+    public static final String deleteStereotypes = "DeleteStereotypes";
+    
+    public static final String createUsers = "CreateUsers";
+    public static final String readUsers = "ReadUsers";
+    public static final String updateUsers = "UpdateUsers";
+    public static final String deleteUsers = "DeleteUsers";
+
+}
