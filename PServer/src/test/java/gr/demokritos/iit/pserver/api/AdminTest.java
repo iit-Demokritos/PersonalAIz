3534e674c736279bcfa11744b750590dcc1d3bf1
panagiotis <panagiotis@panagiotis-iit.demokritos.gr>
2015-06-12 15:35:01 +0300

    PServer - add Admin and Personal unit test class

---
+++ PServer/src/test/java/gr/demokritos/iit/pserver/api/AdminTest.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.demokritos.iit.pserver.api;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Giotis Panagiotis <giotis.p@gmail.com>
+ */
+public class AdminTest {
+    
+    public AdminTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSettings method, of class Admin.
+     */
+    @Test
+    public void testGetSettings() {
+        System.out.println("getSettings");
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.getSettings();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSettings method, of class Admin.
+     */
+    @Test
+    public void testSetSettings() {
+        System.out.println("setSettings");
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.setSettings();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addClient method, of class Admin.
+     */
+    @Test
+    public void testAddClient() {
+        System.out.println("addClient");
+        String clientName = "";
+        String password = "";
+        String JSONClientInfo = "";
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.addClient(clientName, password, JSONClientInfo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteClient method, of class Admin.
+     */
+    @Test
+    public void testDeleteClient() {
+        System.out.println("deleteClient");
+        String clientName = "";
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.deleteClient(clientName);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getClients method, of class Admin.
+     */
+    @Test
+    public void testGetClients() {
+        System.out.println("getClients");
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.getClients();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getClientUID method, of class Admin.
+     */
+    @Test
+    public void testGetClientUID() {
+        System.out.println("getClientUID");
+        String clientName = "";
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.getClientUID(clientName);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setClientRoles method, of class Admin.
+     */
+    @Test
+    public void testSetClientRoles() {
+        System.out.println("setClientRoles");
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.setClientRoles();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ PServer/src/test/java/gr/demokritos/iit/pserver/api/PersonalTest.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.demokritos.iit.pserver.api;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Giotis Panagiotis <giotis.p@gmail.com>
+ */
+public class PersonalTest {
+    
+    public PersonalTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addUsers method, of class Personal.
+     */
+    @Test
+    public void testAddUsers() {
+        System.out.println("addUsers");
+        String JSONUsers = "";
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.addUsers(JSONUsers);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteUsers method, of class Personal.
+     */
+    @Test
+    public void testDeleteUsers() {
+        System.out.println("deleteUsers");
+        String pattern = "";
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.deleteUsers(pattern);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUsers method, of class Personal.
+     */
+    @Test
+    public void testGetUsers() {
+        System.out.println("getUsers");
+        String pattern = "";
+        Integer page = null;
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.getUsers(pattern, page);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setUsersAttributes method, of class Personal.
+     */
+    @Test
+    public void testSetUsersAttributes() {
+        System.out.println("setUsersAttributes");
+        String JSONUsersAttributes = "";
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.setUsersAttributes(JSONUsersAttributes);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUserAttributes method, of class Personal.
+     */
+    @Test
+    public void testGetUserAttributes() {
+        System.out.println("getUserAttributes");
+        String user = "";
+        String pattern = "";
+        Integer page = null;
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.getUserAttributes(user, pattern, page);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setUsersFeatures method, of class Personal.
+     */
+    @Test
+    public void testSetUsersFeatures() {
+        System.out.println("setUsersFeatures");
+        String JSONUsersFeatures = "";
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.setUsersFeatures(JSONUsersFeatures);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of modifyUsersFeatures method, of class Personal.
+     */
+    @Test
+    public void testModifyUsersFeatures() {
+        System.out.println("modifyUsersFeatures");
+        String JSONUsersFeatures = "";
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.modifyUsersFeatures(JSONUsersFeatures);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUserFeatures method, of class Personal.
+     */
+    @Test
+    public void testGetUserFeatures() {
+        System.out.println("getUserFeatures");
+        String user = "";
+        String pattern = "";
+        Integer page = null;
+        Personal instance = null;
+        String expResult = "";
+        String result = instance.getUserFeatures(user, pattern, page);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
