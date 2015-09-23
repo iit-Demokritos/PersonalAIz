eda901d5091a005f409743e3e7c0daf9902c147e
pgiotis <pgiotis@pgiotis-Desktop>
2015-09-23 21:40:04 +0300

    ~PServer 	- complete Update Stereotype Features functionality

--- PServer/src/main/java/gr/demokritos/iit/pserver/PServerTest.java
+++ PServer/src/main/java/gr/demokritos/iit/pserver/PServerTest.java
@@ -25,18 +25,18 @@ public class PServerTest {
 
     public static void main(String[] args) throws IOException, DeserializationException {
 
-//        String clientInfo = "{"
-//                + "\"mail\":\"info@mail.com\","
-//                + "\"info1\":\"info1value\""
-//                + "}";
-//
-//        System.out.println("===============================================");
-//        Client cl =new Client("root", "root");
-//        cl.setAuthenticatedTimestamp(new Date().getTime());
-//         Admin admin= new Admin(new PServerHBase(),cl );
-//         HashMap<String, String> info = new HashMap<>();
-//         
-//         admin.addClient("root", "root", info);
+        String clientInfo = "{"
+                + "\"mail\":\"info@mail.com\","
+                + "\"info1\":\"info1value\""
+                + "}";
+
+        System.out.println("===============================================");
+        Client cl =new Client("root", "root");
+        cl.setAuthenticatedTimestamp(new Date().getTime());
+         Admin admin= new Admin(new PServerHBase(),cl );
+         HashMap<String, String> info = new HashMap<>();
+         
+         admin.addClient("root", "root", info);
 //        String userJSON ="{\"attributes\":{\"gender\":\"male\",\"age\": \"18\"},\"features\": {\"ftr1\": \"34\",\"ftr3\": \"3\",\"ftr5\": \"4\"}}";
 
 
@@ -64,54 +64,54 @@ public class PServerTest {
 //        System.out.println(ps.setUsersFeatures("{\"user1\":{\"category1\":\"top\",\"ftr56\":\"0\"}}"));
 //        System.out.println(ps.modifyUsersFeatures("{\"user1\":{\"ftr1\":\"5\"}}"));
         //Crete new client
-        Client cl = new Client("root", "root");
-        //Set client auth time
-        cl.setAuthenticatedTimestamp(new Date().getTime());
-//        //Create new personal instance
-        Personal instance = new Personal(new PServerHBase(), cl);
-//        instance.deleteUsers(null);
-
-        Random r = new Random(50);
-        ArrayList<User> usersList = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-
-            //Create new User
-            String username = "TestUser" + i;
-            User user = new User(username);
-
-            HashMap<String, String> attributes = new HashMap<>();
-
-            if (r.nextBoolean()) {
-                attributes.put("gender", "male");
-            } else {
-                attributes.put("gender", "female");
-            }
-            attributes.put("age", Integer.toString(r.nextInt(45)));
-            if (r.nextBoolean()) {
-                attributes.put("country", "en");
-            } else {
-                attributes.put("country", "gr");
-            }
-            
-            HashMap<String, String> info = new HashMap<>();
-            info.put("Username", username);
-            user.setInfo(info);
-
-            attributes.put("oc", Integer.toString(r.nextInt(3)));
-
-            user.setAttributes(attributes);
-
-            HashMap<String, String> features = new HashMap<>();
-            features.put("category.sport", Integer.toString(r.nextInt(30)));
-            features.put("category.eco", Integer.toString(r.nextInt(30)));
-            features.put("txt.basket", Integer.toString(r.nextInt(50)));
-            features.put("txt.hellas", Integer.toString(r.nextInt(50)));
-            user.setFeatures(features);
-            System.out.println(username + " --> " + attributes);
-            usersList.add(user);
-        }
-            instance.addUsers(usersList);
+//        Client cl = new Client("root", "root");
+//        //Set client auth time
+//        cl.setAuthenticatedTimestamp(new Date().getTime());
+////        //Create new personal instance
+//        Personal instance = new Personal(new PServerHBase(), cl);
+////        instance.deleteUsers(null);
+//
+//        Random r = new Random(50);
+//        ArrayList<User> usersList = new ArrayList<>();
+//
+//        for (int i = 0; i < 5; i++) {
+//
+//            //Create new User
+//            String username = "TestUser" + i;
+//            User user = new User(username);
+//
+//            HashMap<String, String> attributes = new HashMap<>();
+//
+//            if (r.nextBoolean()) {
+//                attributes.put("gender", "male");
+//            } else {
+//                attributes.put("gender", "female");
+//            }
+//            attributes.put("age", Integer.toString(r.nextInt(45)));
+//            if (r.nextBoolean()) {
+//                attributes.put("country", "en");
+//            } else {
+//                attributes.put("country", "gr");
+//            }
+//            
+//            HashMap<String, String> info = new HashMap<>();
+//            info.put("Username", username);
+//            user.setInfo(info);
+//
+//            attributes.put("oc", Integer.toString(r.nextInt(3)));
+//
+//            user.setAttributes(attributes);
+//
+//            HashMap<String, String> features = new HashMap<>();
+//            features.put("category.sport", Integer.toString(r.nextInt(30)));
+//            features.put("category.eco", Integer.toString(r.nextInt(30)));
+//            features.put("txt.basket", Integer.toString(r.nextInt(50)));
+//            features.put("txt.hellas", Integer.toString(r.nextInt(50)));
+//            user.setFeatures(features);
+//            System.out.println(username + " --> " + attributes);
+//            usersList.add(user);
+//        }
+//            instance.addUsers(usersList);
 
         System.out.println("===============================================");
     }
