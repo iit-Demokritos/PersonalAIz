a82c7a0835290a744bdca9f507682b45bd68dbf7
panagiotis <panagiotis@panagiotis-iit.demokritos.gr>
2015-10-26 15:26:02 +0200

    ~ PersonalAIz - create login page for basic admin panel

---
+++ PersonalAIz/src/main/webapp/js/personalaiz.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function checkCredentials(){
+    var username = $("inputUsername").val;
+    var password = $('inputPassword').val;
+    
+    
+    
+    alert(username +" - "+ password);
+    
+}
